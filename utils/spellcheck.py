23cd2c069ebbf79e54b1a62efe3c15714ea5ae4d
Katherine Yu <kyu@fanatics.com>
2017-02-02 14:19:23 -0800

    init spellcheck

---
+++ utils/spellcheck.py
@@ -0,0 +1,38 @@
+import requests
+import re
+import time
+from random import randint
+
+START_SPELL_CHECK="<span class=\"spell\">Showing results for</span>"
+END_SPELL_CHECK="<br><span class=\"spell_orig\">Search instead for"
+
+HTML_Codes = (
+        ("'", '&#39;'),
+        ('"', '&quot;'),
+        ('>', '&gt;'),
+        ('<', '&lt;'),
+        ('&', '&amp;'),
+)
+
+def spell_check(s):
+    q = '+'.join(s.split())
+    time.sleep(  randint(0,2) ) #relax and don't let google be angry
+    r = requests.get("https://www.google.co.uk/search?q="+q)
+    content = r.text
+    start=content.find(START_SPELL_CHECK) 
+    if ( start > -1 ):
+        start = start + len(START_SPELL_CHECK)
+        end=content.find(END_SPELL_CHECK)
+        search= content[start:end]
+        search = re.sub(r'<[^>]+>', '', search)
+        for code in HTML_Codes:
+            search = search.replace(code[1], code[0])
+        search = search[1:]
+    else:
+        search = s
+    return search ;
+
+# test
+input1 = "Which one dissolve in water quikly sugar, salt, methane and carbon di oxide?"
+output1 = "Which one dissolve in water quickly sugar, salt, methane and carbon di oxide?"
+assert spell_check(input1) == output1
